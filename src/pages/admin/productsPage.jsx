9dbc45dbadf7324e444440c873cb26b88d9e2d68
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-12-06 16:13:25 +0530

    Product delete feature

--- src/pages/admin/productsPage.jsx
+++ src/pages/admin/productsPage.jsx
@@ -3,18 +3,44 @@ import { sampleProducts } from "../../assets/sampleData";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { FaEdit, FaTrash } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 export default function AdminProductsPage() {
   const [products, setProducts] = useState(sampleProducts);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
+    if (isLoading == true) {
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/products")
+        .then((res) => {
+          console.log(res.data);
+          setProducts(res.data);
+          setIsLoading(false);
+        });
+    }
+  }, [isLoading]);
+
+  function deleteProduct(productId) {
+    const token = localStorage.getItem("token");
+    if (token == null) {
+      toast.error("Please login first");
+      return;
+    }
     axios
-      .get(import.meta.env.VITE_BACKEND_URL + "/api/products")
-      .then((res) => {
-        console.log(res.data);
-        setProducts(res.data);
+      .delete(import.meta.env.VITE_BACKEND_URL + "/api/products/" + productId, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then(() => {
+        toast.success("Product Deleted successfully");
+        setIsLoading(true);
+      })
+      .catch((e) => {
+        toast.error(e.response.data.message);
       });
-  }, []);
+  }
 
   return (
     <div className="w-full h-full max-h-full overflow-y-scroll relative">
@@ -24,51 +50,60 @@ export default function AdminProductsPage() {
       >
         +
       </Link>
-      <table className="w-full text-center">
-        <thead>
-          <tr>
-            <th>Product ID</th>
-            <th>Name</th>
-            <th>Image</th>
-            <th>Labelled Price</th>
-            <th>Price</th>
-            <th>Stock</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>{item.productId}</td>
-                <td>{item.name}</td>
-                <td>
-                  <img
-                    src={item.images[0]}
-                    className="w-[50px] h-[50px] object-cover"
-                  />
-                </td>
-                <td>{item.labelledPrice}</td>
-                <td>{item.price}</td>
-                <td>{item.stock}</td>
-                <td>
-                  <div className="flex justify-center items-center">
-                    <FaTrash className="text-[20px] text-red-500 mx-2 cursor-pointer" />
-                    <FaEdit
-                      onClick={() => {
-                        navigate("/admin/edit-product", {
-                          state: item,
-                        });
-                      }}
-                      className="text-[20px] text-blue-500 mx-2 cursor-pointer"
+      {!isLoading ? (
+        <table className="w-full text-center">
+          <thead>
+            <tr>
+              <th>Product ID</th>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Labelled Price</th>
+              <th>Price</th>
+              <th>Stock</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.productId}</td>
+                  <td>{item.name}</td>
+                  <td>
+                    <img
+                      src={item.images[0]}
+                      className="w-[50px] h-[50px] object-cover"
                     />
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                  </td>
+                  <td>{item.labelledPrice}</td>
+                  <td>{item.price}</td>
+                  <td>{item.stock}</td>
+                  <td>
+                    <div className="flex justify-center items-center">
+                      <FaTrash
+                        className="text-[20px] text-red-500 mx-2 cursor-pointer"
+                        onClick={() => {
+                          deleteProduct(item.productId);
+                        }}
+                      />
+                      <FaEdit
+                        onClick={() => {
+                          navigate("/admin/edit-product", {
+                            state: item,
+                          });
+                        }}
+                        className="text-[20px] text-blue-500 mx-2 cursor-pointer"
+                      />
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
   );
 }
