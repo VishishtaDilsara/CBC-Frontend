747a54d47c6e65c84270cc6db13e23d8fe628cca
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-12-07 10:51:56 +0530

    Product Overview page created

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -33,7 +33,51 @@ export default function ProductOverviewPage() {
           <div className="w-[50%] h-full flex justify-center items-center">
             <ImageSlider images={product.images} />
           </div>
-          <div className="w-[50%] h-full bg-blue-900"></div>
+          <div className="w-[50%] h-full flex justify-center items-center">
+            <div className="w-[600px] h-[500px] flex flex-col  items-center">
+              <h1 className="w-full text-center text-3xl font-semibold text-secondary">
+                {product.name}
+                {product.altNames.map((altName, index) => {
+                  return (
+                    <span
+                      key={index}
+                      className="text-gray-600 font-normal text-3xl"
+                    >
+                      {" | " + altName}
+                    </span>
+                  );
+                })}
+              </h1>
+              <h1 className="w-full text-center text-md text-gray-600 font-semibold my-2">
+                {product.productId}
+              </h1>
+              <p className="w-full text-center text-md text-gray-600 font-semibold my-2">
+                {product.description}
+              </p>
+              {product.labelledPrice > product.price ? (
+                <div>
+                  <span className="text-4xl mx-4 text-gray-500 line-through">
+                    {product.labelledPrice.toFixed(2)}
+                  </span>
+                  <span className="text-4xl mx-4 font-bold text-accent">
+                    {product.price.toFixed(2)}
+                  </span>
+                </div>
+              ) : (
+                <span className="text-4xl mx-4 font-bold text-accent">
+                  {product.price.toFixed(2)}
+                </span>
+              )}
+              <div className="w-full flex justify-center items-center mt-4">
+                <button className="w-[200px] h-[50px] mx-4 bg-accent cursor-pointer text-white font-semibold rounded-2xl hover:bg-accent/80 transition-all duration-300">
+                  Add to Cart
+                </button>
+                <button className="w-[200px] h-[50px] mx-4 bg-accent cursor-pointer text-white font-semibold rounded-2xl hover:bg-accent/80 transition-all duration-300">
+                  Buy Now
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       )}
       {status == "loading" && <Loading />}
