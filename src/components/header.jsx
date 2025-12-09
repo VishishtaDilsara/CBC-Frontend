7121f8db2135e9da9015c252e97aac3a25de6fad
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-12-09 22:01:08 +0530

    Hamburger icon

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,12 @@
 import { BsCart3 } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const navigate = useNavigate();
   return (
-    <header className="w-full h-[80px] shadow-2xl flex">
+    <header className="w-full h-[80px] shadow-2xl flex justify-center relative">
+      <GiHamburgerMenu className="h-full mx-4 text-3xl md:hidden absolute left-2" />
       <img
         onClick={() => {
           navigate("/");
@@ -13,7 +15,7 @@ export default function Header() {
         alt="logo"
         className="w-[70px] h-[70px]  top-0 left-0 m-2 object-cover cursor-pointer"
       />
-      <div className="w-[calc(100%-160px)] h-full flex justify-center items-center">
+      <div className="w-[calc(100%-160px)] hidden md:flex h-full flex justify-center items-center">
         <Link to="/" className=" text-[20px] font-bold mx-2">
           Home
         </Link>
@@ -27,7 +29,7 @@ export default function Header() {
           Contact
         </Link>
       </div>
-      <div className="w-[80px] flex justify-center items-center">
+      <div className="w-[80px] hidden md:flex flex justify-center items-center">
         <Link to="/cart" className=" text-[20px] font-bold mx-2">
           <BsCart3 />
         </Link>
--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,34 +1,5 @@
-import { useState } from "react";
-import mediaUpload from "../utils/mediaUpload";
-
 export default function TestPage() {
-  const [image, setImage] = useState(null);
-
-  function fileUpload() {
-    mediaUpload(image)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((res) => {
-        console.log(res);
-      });
-  }
-
   return (
-    <div className="w-full h-screen flex justify-center items-center flex-col">
-      <input
-        type="file"
-        className="file-input file-input-bordered w-full max-w-xs"
-        onChange={(e) => {
-          setImage(e.target.files[0]);
-        }}
-      />
-      <button
-        onClick={fileUpload}
-        className="bg-green-500 text-white font-bold py-2 px-4 rounded"
-      >
-        Upload
-      </button>
-    </div>
+    <div className="w-full h-screen flex justify-center items-center flex-col md:bg-amber-800 lg:bg-amber-500"></div>
   );
 }
