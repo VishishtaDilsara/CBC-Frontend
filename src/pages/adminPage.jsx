2538f5ace0e61aab2790962d1df3291f506809e4
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-12-09 09:26:43 +0530

    Admin page navigation bg created

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,16 +1,31 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 
 import AddProductPage from "./admin/addProductPage";
 import AdminProductsPage from "./admin/productsPage";
 import EditProductPage from "./admin/editProductPage";
 
 export default function AdminPage() {
+  const location = useLocation();
+  const path = location.pathname;
+  function getClass(name) {
+    if (path.includes(name)) {
+      return "bg-accent text-white";
+    } else {
+      return "text-accent";
+    }
+  }
   return (
     <div className="w-full h-screen flex">
-      <div className="h-full w-[300px] flex flex-col">
-        <Link to="/admin/products">Products</Link>
-        <Link to="/admin/users">Users</Link>
-        <Link to="/admin/orders">Orders</Link>
+      <div className="h-full w-[300px] text-accent font-bold px-4 text-xl gap-5 flex flex-col">
+        <Link className={getClass("products")} to="/admin/products">
+          Products
+        </Link>
+        <Link className={getClass("users")} to="/admin/users">
+          Users
+        </Link>
+        <Link className={getClass("orders")} to="/admin/orders">
+          Orders
+        </Link>
       </div>
       <div className="h-full w-[calc(100%-300px)] ">
         <Routes path="/*">
